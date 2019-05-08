d97b21b0ef08329bd4e7d96fa5c22f388afd7007
PeiChing Chen <thatsall0519@gmail.com>
2019-05-08 22:05:27 +0800

    190509 建立變數

---
+++ js/index.js
@@ -0,0 +1,21 @@
+var msg = "Hello world"
+
+//msg+=""; 等同於msg = msg + "";
+
+// /*只有一個"確認"按鈕*/
+// alert('hello world!');
+
+// /*有確認與取消按鈕*/
+// conffirm("好啊");
+
+// var hello = "hello world ! ";
+// var foo = " foobar~";
+// var msg = hello + foo ;
+// alert(msg);
+
+var a=5;
+var b=6;
+var c="9.7";
+//parseInt是無條件捨去
+var m = a + b + parseInt(c);
+alert(m);
